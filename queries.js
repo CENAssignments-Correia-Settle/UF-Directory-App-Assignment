212a69a5d17ec11958c6451d13421acd1273538b
Jonathen Settle <settlejonathen@yahoo.com>
2016-01-20 22:05:12 -0500

    retrieveAllListings appears to be working

--- queries.js
+++ queries.js
@@ -56,6 +56,13 @@ var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+
+   Listing.find({}, function(err, listings) {
+   		if (err) throw err;
+
+   		console.log(listings);
+   });
+
 };
 
 findLibraryWest();
